03ad55e8edf11d89bf792a28c759ee79664b7a0d
anhtudo97 <anhtudo97@gmail.com>
2021-08-06 23:50:55 +0700

    feat: list

---
+++ src/components/List/index.tsx
@@ -0,0 +1,63 @@
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
+import { useRef, useState } from "react";
+import { ListItem } from "../ListItem";
+import "./style.scss";
+
+export const List = () => {
+  const [isMoved, setIsMoved] = useState<boolean>(false);
+  const [slideNumber, setSlideNumber] = useState<number>(0);
+
+  const listRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = (direction: number | string) => {
+    setIsMoved(true);
+    let distance = 0;
+    if (listRef.current) {
+      distance = listRef.current.getBoundingClientRect().x - 50;
+    }
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      if (listRef.current) {
+        listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      }
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      if (listRef.current) {
+        listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      }
+    }
+  };
+
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved ? "none" : "block" }}
+        />
+        <div className="container">
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
+        </div>
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+};
